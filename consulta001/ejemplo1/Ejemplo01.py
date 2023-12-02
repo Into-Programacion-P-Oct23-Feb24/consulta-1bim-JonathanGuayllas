09b85429837988046a4093b87a4cdb996a639784
Jonathan <jdguayllas@utpl.edu.ec>
2023-12-01 20:02:30 -0500

    Consulta001

---
+++ consulta001/ejemplo1/Ejemplo01.py
@@ -0,0 +1,6 @@
+num1 = int(input("Ingrese el primer número "))
+num2 = int(input("Ingrese el segundo número "))
+num3 = int(input("Ingrese el tercer número "))
+
+cadena = "Los numeros ingresados son: {} - {} - {}".format(num1, num2, num3)
+print(cadena)
---
+++ consulta001/ejemplo2/Ejemplo02.py
@@ -0,0 +1,7 @@
+nombre = str(input("Ingrese su nombre: "))
+apellido = str(input("Ingrese su apellido: "))
+edad = int(input("Ingrese su edad: "))
+
+cadena = "Los datos ingresados son:\nNombre: {}\nApellido: {}\nEdad: {}".format(nombre, apellido, edad)
+
+print(cadena)
